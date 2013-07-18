338c354f9a3ceb3ebb22535141cc1e850f116cd0
Lixu <nicholaslixu@gmail.com>
2013-07-18 11:45:44 +0800

    add runable header file

---
+++ Fundation/include/Runable.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: Runable.cpp
+	> Author: nicholas
+	> Mail:nicholaslixu@gmail.com 
+	> Created Time: 四  7/18 11:37:24 2013
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+namespace lixu_network
+{
+	class Runable
+	{
+		public:
+			virtual void runable();
+	};
+}
